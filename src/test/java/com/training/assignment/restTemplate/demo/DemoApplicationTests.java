ec10c5e92ba099e270cc028c9b61fb43cd6683b0
Richard Strickler <richastrick1998@gmail.com>
2022-09-07 08:52:38 -0500

    initial

---
+++ src/test/java/com/training/assignment/restTemplate/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.assignment.restTemplate.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
